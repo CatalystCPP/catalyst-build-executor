f55ee3edd3d766a313c989b73290dcbc43a90020
Siddharth Mohanty <neosiddharth@gmail.com>
2026-08-04 12:14:29 -0700

    fix: refactor FileDescriptor class

---
+++ include/cob/file_descriptor.hpp
@@ -0,0 +1,34 @@
+#include <memory>
+#include <unistd.h>
+
+namespace catalyst {
+class FileDescriptor {
+public:
+    explicit FileDescriptor(int descriptor) : descriptor(descriptor) {
+    }
+
+    ~FileDescriptor() {
+        if (descriptor >= 0) {
+            ::close(descriptor);
+        }
+    }
+
+    FileDescriptor(const FileDescriptor &) = delete;
+    FileDescriptor &operator=(const FileDescriptor &) = delete;
+    FileDescriptor(FileDescriptor &&) = delete;
+    FileDescriptor &operator=(FileDescriptor &&) = delete;
+
+    [[nodiscard]] int get() const {
+        return descriptor;
+    }
+
+    bool close() {
+        int descriptor_to_close = descriptor;
+        descriptor = -1;
+        return ::close(descriptor_to_close) == 0;
+    }
+
+private:
+    int descriptor;
+};
+} // namespace catalyst
--- src/binary.cpp
+++ src/binary.cpp
@@ -6,12 +6,14 @@
 #include "cob/flat_map.hpp"
 #include "cob/graph.hpp"
 #include "cob/optional_vector.hpp"
+#ifdef __linux__
+#include "cob/file_descriptor.hpp"
+#endif
 
 #include <array>
 #include <cerrno>
 #include <cstdint>
 #include <cstring>
-#include <fstream>
 #include <memory>
 #include <string_view>
 #include <vector>
@@ -71,38 +73,6 @@ struct BinDefinition {
     StringRef key;
     StringRef val;
 };
-
-#ifdef __linux__
-class FileDescriptor {
-public:
-    explicit FileDescriptor(int descriptor) : descriptor(descriptor) {
-    }
-
-    ~FileDescriptor() {
-        if (descriptor != -1) {
-            ::close(descriptor);
-        }
-    }
-
-    FileDescriptor(const FileDescriptor &) = delete;
-    FileDescriptor &operator=(const FileDescriptor &) = delete;
-    FileDescriptor(FileDescriptor &&) = delete;
-    FileDescriptor &operator=(FileDescriptor &&) = delete;
-
-    [[nodiscard]] int get() const {
-        return descriptor;
-    }
-
-    bool close() {
-        int descriptor_to_close = descriptor;
-        descriptor = -1;
-        return ::close(descriptor_to_close) == 0;
-    }
-
-private:
-    int descriptor;
-};
-#endif
 
 Result<void> writeBinData(const BinHeader &header,
                           const std::vector<BinDefinition> &bin_defs,
